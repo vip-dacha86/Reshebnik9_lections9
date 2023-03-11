48753307f7c3acbb601f18e6cc4e8a35d54c4729
vip-dacha86 <vip-dacha86@yandex.ru>
2023-03-11 12:42:36 +0500

    task67_N=sum

---
+++ task67/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу которая будет принимать на вход число и возвращать сумму его чисел.
+// 453=>12      45=>9
+using System;
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int m = int.Parse(ReadLine());
+WriteLine($"{m}->{SumNumbers(m)}");
+int sum = 0;
+while(m>0) {
+    sum += m % 10; // <=> sum = sum +m % 10
+    m/=10;         //<=> m = m/10
+}
+WriteLine($"{sum}");
+int SumNumbers(int number)
+{
+    if (number == 0)
+    return 0;
+    return (number%10+SumNumbers(number/10));
+}
+
