17b36dc3209490c541108cc3e8636fefcbb8b2e4
vip-dacha86 <vip-dacha86@yandex.ru>
2023-03-11 10:06:15 +0500

    task63_числа от 1 до N

---
+++ task63/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N .Напишите программу которая выведет все натуральные числа в промежутке от 1 до N.
+// N=5->1,2,3,4,5/    N->1,2,3,4,5,6/
+using System;
+using static System.Console;
+Clear();
+Write("Введите N: ");
+int n=int.Parse(ReadLine());
+for (int i = 1; i <= n;i++){
+    Write($"{i} ");
+}
+WriteLine();
+WriteLine(PrintNumbers(n));
+String PrintNumbers(int n){
+    if (n==1)
+    {
+        WriteLine(1);
+        return "1";
+    }
+    String s = PrintNumbers (n-1)+" "+n.ToString();
+    WriteLine(s);
+    return s;
+}
